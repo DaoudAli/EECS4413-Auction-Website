22240cedd7858c061e411df365329d6db16a7254
Tahas022 <taha.shahid70@gmail.com>
2023-11-02 23:49:19 -0400

    added restcontroller with api calls

--- UserServiceApp/src/main/java/com/EECS4413/UserServiceApp/controller/UserController.java
+++ UserServiceApp/src/main/java/com/EECS4413/UserServiceApp/controller/UserController.java
@@ -1,5 +1,23 @@
 package com.EECS4413.UserServiceApp.controller;
 
+import java.util.List;
+import org.springframework.web.bind.annotation.*;
+import com.EECS4413.UserServiceApp.model.User;
+import com.EECS4413.UserServiceApp.services.UserServices;
+
+@RestController
 public class UserController {
+
+	private UserServices userServices = new UserServices();
+
+	@GetMapping("/users")
+	public List<User> getAllUsers() {
+		return userServices.readAll();
+	}
+
+	@PostMapping("/users")
+	public User createNewUser(User user) {
+		return userServices.create(user);
+	}
 
 }
