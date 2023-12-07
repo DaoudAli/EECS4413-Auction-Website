add607b40709c4b251b407c50ded3bd1f9b47c24
kingsley <king808@my.yorku.ca>
2023-12-07 15:35:37 -0500

    feat(client-app): Add AuctionDetails component
    This commit adds the AuctionDetails component to improve the user
    experience in the client app. The component displays key details of an
    auction, including type, status, and time left. It handles bid placement
    for Forward auctions and 'Buy Now' functionality for Dutch auctions. The
    component utilizes hooks to access necessary data and functionalities,
    calculates time remaining, and computes total costs. It also checks
    eligibility for payment. Overall, the AuctionDetails component enhances
    auction interactions.

--- client-app/src/context/AuctionContext.js
+++ client-app/src/context/AuctionContext.js
@@ -43,28 +43,94 @@ export const AuctionProvider = ({ children }) => {
     try {
       const response = await auctionServiceApi.get(`/items/${itemId}`);
       setCurrentAuction(response.data);
+      return response.data;
     } catch (error) {
       console.error(`Error fetching auction with ID ${itemId}:`, error);
     } finally {
       setIsLoading(false);
     }
   }, []);
+  const placeBid = async (auctionId, amount, bidderId) => {
+    try {
+      const bid = {
+        amount,
+        bidderId,
+      };
+      const response = await auctionServiceApi.post(`/${auctionId}/bids`, bid);
+      console.log('Bid placed:', response.data);
+      return response.data;
+      // You can handle the response here, e.g., update the current auction details
+    } catch (error) {
+      console.error(`Error placing bid on auction ${auctionId}:`, error);
+      // Handle error
+    }
+  };
 
-  const connectWebSocket = useCallback((auctionId) => {
-    const url = `ws:${BASE_URL}/${auctionId}/update`; // Replace BASE_URL with actual base URL
-    const socket = new WebSocket(url);
-    // WebSocket event handlers...
-    setWebSocket(socket);
-    return () => socket.close();
-  }, []);
+  // Function to execute 'Buy Now' for a Dutch auction
+  const buyNow = async (auctionId, amount, bidderId) => {
+    try {
+      const bid = {
+        amount,
+        bidderId,
+      };
+      const response = await auctionServiceApi.post(
+        `/${auctionId}/buy-now`,
+        bid
+      );
+      console.log('Item purchased:', response.data);
+      return response.data;
+      // Handle the response here, e.g., update the auction status
+    } catch (error) {
+      console.error(
+        `Error executing 'Buy Now' on auction ${auctionId}:`,
+        error
+      );
+      // Handle error
+    }
+  };
+
+  const connectWebSocket = useCallback(
+    (auctionId) => {
+      const url = `ws:${process.env.NEXT_PUBLIC_WS_URL}/${auctionId}/update`;
+      console.log('Inside connect websocket', url);
+      // Create a new WebSocket connection
+      const socket = new WebSocket(url);
+
+      // Set up WebSocket event handlers
+      socket.onopen = () => {
+        console.log('WebSocket connection established');
+      };
+
+      socket.onmessage = (event) => {
+        try {
+          const updatedAuctionInfo = JSON.parse(event.data);
+          // You might need to handle the received data appropriately
+          console.log('Received message:', updatedAuctionInfo);
+        } catch (e) {
+          console.error('Error parsing message:', e);
+        }
+      };
+
+      socket.onerror = (error) => {
+        console.error('WebSocket error:', error);
+      };
+
+      // Store the WebSocket connection in state or context
+      setWebSocket(socket);
+
+      // Return the connected WebSocket object
+      return socket;
+    },
+    [setWebSocket]
+  );
 
   const disconnectWebSocket = useCallback(() => {
-    // Function to disconnect the WebSocket
-    if (webSocket) {
-      webSocket.close();
-      setWebSocket(null);
+    // Check if the WebSocket is established and open
+    if (webSocket && webSocket.readyState === WebSocket.OPEN) {
+      webSocket.close(); // Close the WebSocket connection
+      console.log('WebSocket connection closed');
     }
-  }, [webSocket]);
+  }, [webSocket]); // Depend on the webSocket state
 
   // Add other API methods (POST, DELETE) here...
 
@@ -74,6 +140,9 @@ export const AuctionProvider = ({ children }) => {
         auctions,
         currentAuction,
         isLoading,
+        webSocket,
+        placeBid,
+        buyNow,
         fetchAuctions,
         getAuctionById,
         getAuctionByItemId,
