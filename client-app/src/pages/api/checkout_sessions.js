3fbe689c40662d9398e3d00af2da56c9100d797f
Daoud Ali <65147757+DaoudAli@users.noreply.github.com>
2023-12-06 22:47:31 -0500

    Stripe changes

---
+++ client-app/src/pages/api/checkout_sessions.js
@@ -0,0 +1,51 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  switch (req.method) {
+    case "POST":
+      try {
+        // Assuming the items are sent in the request body
+        const { items } = req.body; // items should be an array of objects with {name, amount, currency, quantity}
+
+        // Dynamically create lineItems for stripe
+        const lineItems = items.map((item) => ({
+          price_data: {
+            currency: "CAD",
+            product_data: {
+              name: item.itemName,
+            },
+            unit_amount: item.itemPrice,
+          },
+          quantity: item.quantity,
+        }));
+
+        // Create Checkout Sessions from body params.
+        const session = await stripe.checkout.sessions.create({
+          ui_mode: "embedded",
+          line_items: lineItems,
+          mode: "payment",
+          return_url: `http://localhost:3000/payment/receipt`,
+        });
+
+        res.send({ clientSecret: session.client_secret });
+      } catch (err) {
+        res.status(err.statusCode || 500).json(err.message);
+      }
+    case "GET":
+      try {
+        const session = await stripe.checkout.sessions.retrieve(
+          req.query.session_id
+        );
+
+        res.send({
+          status: session.status,
+          customer_email: session.customer_details.email,
+        });
+      } catch (err) {
+        res.status(err.statusCode || 500).json(err.message);
+      }
+    default:
+      res.setHeader("Allow", req.method);
+      res.status(405).end("Method Not Allowed");
+  }
+}
--- client-app/src/pages/payment/index.js
+++ client-app/src/pages/payment/index.js
@@ -1,77 +1,54 @@
-export default function Payment() {
+import React, { useEffect, useState } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout,
+} from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe(
+  "pk_test_51OKUz3GghcrNY4DgU2cNCU5iFabyzHUz6HDzew97l768AnMQXqAoTxZGNhQt5ZD4uPRbC3KAWZ7PGDBKUeS12JjB00t0yLXhGz"
+);
+
+export default function App() {
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    let userCartItems = localStorage.getItem("userCartItems");
+
+    if (!userCartItems) {
+      userCartItems = [
+        {
+          itemName: "Car",
+          itemPrice: 2309,
+          quantity: 1,
+        },
+      ];
+    }
+    fetch("/api/checkout_sessions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ items: userCartItems }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret))
+      .catch((error) => {
+        // Handle any errors
+        console.error("Error:", error);
+      });
+  }, []);
+
   return (
-    <div className="min-h-screen flex items-center justify-center p-4">
-      <div className="w-full max-w-4xl bg-white rounded-lg shadow-md p-6 md:p-8">
-        <h2 className="text-3xl font-bold text-center text-gray-900 mb-6">
-          Payment Page
-        </h2>
-        <form className="flex flex-wrap -mx-3"
-          onSubmit={e => {
-            console.log()
-            e.preventDefault();
-            fetch("http://localhost:3500/payment/pay", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "http://localhost:3000",
-              },
-              body: JSON.stringify({
-                cardNumber: e.currentTarget.cardNumber.value,
-                nameOnCard: e.currentTarget.nameOnCard.value,
-                expirationDate: e.currentTarget.expirationDate.value,
-                securityCode: e.currentTarget.securityCode.value,
-              }),
-            })
-          }}>
-          <div className="w-full md:w-1/2 px-3 mb-6">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="cardNumber">Card Number *</label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              required id="cardNumber" type="text" placeholder="" maxLength={16} pattern="[0-9]{16}"
-              onInvalid={e => e.target.setCustomValidity("Please enter a valid Card Number")}
-              onChange={e => e.target.setCustomValidity("")} />
-          </div>
-          <div className="w-full md:w-1/2 px-3 mb-6">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="nameOnCard">Name on Card *</label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
-              required id="nameOnCard" type="text" placeholder="" />
-          </div>
-          <div className="w-full md:w-1/2 px-3 mb-6">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="expirationDate">{"Expiration Date (MM/YY) *"}</label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="expirationDate" type="text" pattern="(?:0[1-9]|1[0-2])/[0-9]{2}" maxLength={5}
-              required title="Enter a date in this format MM/YY"
-              onInvalid={e => e.target.setCustomValidity("Please enter a valid Expiration Date")}
-              onChange={e => e.target.setCustomValidity("")} />
-          </div>
-          <div className="w-full md:w-1/2 px-3 mb-6">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="securityCode">{"Security Code (CCV) *"}</label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
-              required id="securityCode" type="text" placeholder="" pattern="[0-9]{3}" maxLength={3}
-              onInvalid={e => e.target.setCustomValidity("Please enter a valid Security Code")}
-              onChange={e => e.target.setCustomValidity("")} />
-          </div>
-          <div className="w-full px-3 mt-6">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
-      </div>
+    <div id="checkout">
+      {clientSecret && (
+        <EmbeddedCheckoutProvider
+          stripe={stripePromise}
+          options={{ clientSecret }}
+        >
+          <EmbeddedCheckout />
+        </EmbeddedCheckoutProvider>
+      )}
     </div>
   );
 }
