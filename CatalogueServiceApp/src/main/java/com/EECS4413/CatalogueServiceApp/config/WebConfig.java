bcc7ad80fd8ae0bafaa1b60f7ef7a875ffc95f17
kingsley <king808@my.yorku.ca>
2023-12-08 19:12:32 -0500

    Refactor CORS configuration to allow all origins and disable credentials

--- CatalogueServiceApp/src/main/java/com/EECS4413/CatalogueServiceApp/config/WebConfig.java
+++ CatalogueServiceApp/src/main/java/com/EECS4413/CatalogueServiceApp/config/WebConfig.java
@@ -14,10 +14,11 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Apply CORS to all endpoints
-                        .allowedOrigins("http://localhost:3000", "https://eecs-4413-auction-website.vercel.app")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE") // HTTP methods allowed
                         .allowedHeaders("*") // All headers allowed
-                        .allowCredentials(true);
+                        .allowCredentials(false);
+
             }
         };
     }
